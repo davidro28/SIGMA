0a3be3d59c87ad9345bb941b133cf3a85c3422f8
David28 <davidrojassalas28@gmail.com>
2025-11-26 20:14:12 -0500

    Organizando rutas

---
+++ src/Paginas/Home/Home.jsx
@@ -0,0 +1,8 @@
+export default function MiHome() {
+    return (
+        <div style={{ padding: "20px" }}>
+        <h1>Mi Página</h1>
+        <p>Aquí puedes escribir el contenido de tu nueva página.</p>
+        </div>
+    );
+}
