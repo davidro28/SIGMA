cf13a22330ff8dc316576dce38514468c3cc3d90
Darcy Mateus <juandavidtobarmartinez9@gmail.com>
2025-12-02 19:48:12 -0500

    creacion de recupero contraseña

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Paginas/Login/login.jsx";
 import Register from "./Paginas/Registro/registro.jsx";
+import ForgotPassword from "./Paginas/recuperar_contraseña/recuperar_contraseña.jsx"; 
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/recuperar_contraseña" element={<ForgotPassword />} />
       <Route path="*" element={<Login />} /> {/* fallback */}
     </Routes>
   );
