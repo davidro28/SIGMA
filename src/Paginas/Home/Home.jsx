f7d4b7221c7926e6852d458e2795ec195298459b
David28 <davidrojassalas28@gmail.com>
2025-12-03 21:07:12 -0500

    Creando Home Administrador

--- src/Paginas/Home/Home.jsx
+++ src/Paginas/Home/Home.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import SigmaHeader from '../../Components/sigmaHeader';
 import VerticalNav from '../../Components/verticalNav';
+import './Home.css';
 
 function MiHome() {
+    const menuItems = [
+        { to: "/general", label: "General" },
+        { to: "/activos", label: "Activos" },
+        { to: "/tickets", label: "Tickets" },
+        { to: "/mantenimiento", label: "Mantenimiento" }
+    ];
     return (
-        <div>
+        <div className="home-container">
             <header>
                 <SigmaHeader />
             </header>
-            <nav>
-                <VerticalNav />
-            </nav>
-            <main>
-                <section></section>
-            </main>
-            <footer></footer>
+            <div className="layout">
+                <VerticalNav items={menuItems} />
+                <main className="content">
+                    <section>
+                        <p className='parrafo_principal'>Vista principal</p>
+                    </section>
+                </main>
+            </div>
         </div>
     );
 }
