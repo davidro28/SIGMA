648bd022c6f6b915bedccc7b738b8700e5b0f6b9
Juan296-Tb <juandavidtobamartinez9@gmail.com>
2025-12-05 00:35:11 -0500

    Activos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MiHome from "./Paginas/Home/Home.jsx";
 import Login from "./Paginas/Login/login.jsx";
 import Register from "./Paginas/Registro/registro.jsx";
 import ForgotPassword from "./Paginas/recuperar_contraseña/recuperar_contraseña.jsx";
+import Activos from "./Paginas/Home/Activos-admin/Activos.jsx"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<MiHome />} />
       <Route path="/recuperar_contraseña" element={<ForgotPassword />} />
+      <Route path="/Activos" element={<Activos />} />
 
       {/* fallback: si no coincide, ir a login */}
       <Route path="*" element={<Login />} />
